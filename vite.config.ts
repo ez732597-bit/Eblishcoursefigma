8d7f3859949737795c0b6549514331a2c9e8b735
ez732597-bit <ez732597@gmail.com>
2025-12-20 14:24:06 +0300

    add correct vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import { resolve } from "path"
 
 export default defineConfig({
     base: '/Eblishcoursefigma/',
     plugins: [react()],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'),
-        },
-    },
-    css: {
-        postcss: './postcss.config.mjs',
-    },
     build: {
         outDir: 'dist',
         emptyOutDir: true,
-        target: 'es2020',
-        minify: 'terser',
-        sourcemap: false,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['react', 'react-dom'],
-                },
-            },
-        },
     },
 })
-
